4c6407c30e708dac48a149c974cd24941f469fca
ToffiCodes <tevfik.kohl@gmail.com>
2024-04-10 15:22:04 +0200

    test

--- QuizBox/src/App.vue
+++ QuizBox/src/App.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Hallo</h1>
+  <main>
+    <h1>QuizBox</h1>
+    <div class="top">Quiz-Boxes</div>
+    <div class="category">Category</div>
+    <div class="category" id="webDev">Web Dev Foundation</div>
+    <div class="category" id="coding">Coding Foundation</div>
+    <div class="category" id="webApp">Web Apps Foundation</div>
+    <div class="category" id="backends">Restful Backends</div>
+    <div class="category" id="vue">Web Apps with VueJS</div>
+    <button class="begin">Start!</button>
+  </main>
 </template>
+
+<style>
+@import url('@/assets/global.css');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: monospace;
+}
+
+body {
+  background-color: black;
+  color: whitesmoke;
+}
+</style>
